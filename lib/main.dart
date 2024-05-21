a5118dd1f82145c62e90eb52316ab49543f03605
Niek1610 <Niek.huisman2002@gmail.com>
2024-05-21 14:38:33 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:linqapp/pages/home.dart';
 import 'services/firebase_options.dart';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
